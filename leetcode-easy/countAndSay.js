bf9195ad596a757dd85378820eb646178493b080
chefthomas <thomcdixon@gmail.com>
2019-05-16 21:30:54 -0700

    add leet-easy count and say

---
+++ leetcode-easy/countAndSay.js
@@ -0,0 +1,34 @@
+
+/**
+ * https://leetcode.com/problems/count-and-say/
+ * @param {number} n
+ * @return {string}
+ */
+const countAndSay = function(n) {
+    
+    if(n === 1) return '1';
+
+    let output = '1';
+    
+    for(let i = 0; i < n - 1; i++){
+        output = rep(output);
+    }
+
+    function rep(str) {
+        
+        let trans = ''
+        let count = 1;
+
+        for(let i = 0; i < str.length; i++) {
+            if(str[i] === str[i + 1]){
+                count++;
+            } else {
+                trans = trans + count.toString(10) + str[i];
+                count = 1;
+            }
+        }
+        return trans;
+    }
+
+    return output;
+};
