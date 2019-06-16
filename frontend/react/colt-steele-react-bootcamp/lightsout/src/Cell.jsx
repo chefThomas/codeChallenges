cf87986195cde504441d1cc0084811e98f548e17
chefthomas <thomcdixon@gmail.com>
2019-06-15 22:48:08 -0700

    add newspaper and style lightout

--- frontend/react/colt-steele-react-bootcamp/lightsout/src/Cell.jsx
+++ frontend/react/colt-steele-react-bootcamp/lightsout/src/Cell.jsx
@@ -1,22 +1,24 @@
-import React, { Component } from 'react';
-import './Cell.css'
+import React, { Component } from "react";
+import "./Cell.css";
 
 export default class Cell extends Component {
-    constructor(props) {
-        super(props);
-        this.ref = React.createRef();
-    }
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
 
-    handleClick = (index) => {
-        this.props.handleClick(this.props.dataindex);
-        // target with ref
-    }
+  handleClick = index => {
+    this.props.handleClick(this.props.dataindex);
+    // target with ref
+  };
 
-
-    render() {
-        return (
-            <div ref={this.ref} className={this.props.className} onClick={this.handleClick}>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div
+        ref={this.ref}
+        className={this.props.className}
+        onClick={this.handleClick}
+      />
+    );
+  }
 }
